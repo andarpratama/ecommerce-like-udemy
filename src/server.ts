08f4bef8c747adbfbf207ed022a45ff0acc5880c
Andar Pratama <master.amarta@gmail.com>
2021-05-10 23:28:34 +0700

    error 07

--- src/server.ts
+++ src/server.ts
@@ -8,30 +8,30 @@ import dotenv from 'dotenv'
 class App {
    public app: Application
    constructor() { 
-      dotenv.config()
       this.app = express()
       this.route()
       this.plugin()
+      dotenv.config()
    }
 
    protected plugin(): void{
-      ConnectMongoDB()
       this.app.use(cors())
       this.app.use(express.json())
       this.app.use(express.urlencoded({ extended: true }))
+      ConnectMongoDB()
 
-      this.app.use((req : Request, res: Response, next: NextFunction) => {
-         res.setHeader("Access-Control-Allow-Origin", "*");
-         res.setHeader(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-         );
-         res.setHeader(
-            "Access-Control-Allow-Methods",
-            "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-         );
-         next();
-      });
+      // this.app.use((req : Request, res: Response, next: NextFunction) => {
+      //    res.setHeader("Access-Control-Allow-Origin", "*");
+      //    res.setHeader(
+      //       "Access-Control-Allow-Headers",
+      //       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+      //    );
+      //    res.setHeader(
+      //       "Access-Control-Allow-Methods",
+      //       "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+      //    );
+      //    next();
+      // });
    }
 
    protected route(): void{
