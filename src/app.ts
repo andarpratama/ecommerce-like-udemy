b624210400f0db4c5b59e212439f2b4a536955d3
Andar Pratama <master.amarta@gmail.com>
2021-05-10 22:19:24 +0700

    error 02

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,4 @@ import config from './config/config';
 
 const app = new App().app
 const NAMESPACE:string = 'SERVER'
-app.listen(process.env.SERVER_PORT, () => logging.info(NAMESPACE, `Server is running http://${config.server.hostname}:${config.server.port}`))
+app.listen(process.env.PORT, () => logging.info(NAMESPACE, `Server is running http://${config.server.hostname}:${process.env.PORT}}`))
