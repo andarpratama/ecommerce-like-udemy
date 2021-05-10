7c124ab3fa514224d9d05ba2a5db600511a28385
Andar Pratama <master.amarta@gmail.com>
2021-05-11 02:20:45 +0700

    add res.setHeader

--- src/server.ts
+++ src/server.ts
@@ -20,18 +20,18 @@ class App {
       this.app.use(express.urlencoded({ extended: true }))
       ConnectMongoDB()
 
-      // this.app.use((req : Request, res: Response, next: NextFunction) => {
-      //    res.setHeader("Access-Control-Allow-Origin", "*");
-      //    res.setHeader(
-      //       "Access-Control-Allow-Headers",
-      //       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-      //    );
-      //    res.setHeader(
-      //       "Access-Control-Allow-Methods",
-      //       "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-      //    );
-      //    next();
-      // });
+      this.app.use((req : Request, res: Response, next: NextFunction) => {
+         res.setHeader("Access-Control-Allow-Origin", "*");
+         res.setHeader(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+         );
+         res.setHeader(
+            "Access-Control-Allow-Methods",
+            "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+         );
+         next();
+      });
    }
 
    protected route(): void{
